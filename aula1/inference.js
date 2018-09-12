0bb4c03fd9aede3aca70f75e1b46d80958fc0534
Jeferson Job <jeferson.job@outlook.com>
2018-09-12 09:35:45 -0400

    First commit in AngularJS 5 course

---
+++ aula1/inference.js
@@ -0,0 +1,15 @@
+function showLogger(message) {
+    console.log(message);
+}
+showLogger("Hello world");
+showLogger(23);
+/*
+string
+number
+boolean
+array
+Class
+*/
+var text = ['Hello', 'Teste'];
+var isEnabled = true;
+var text2 = ['Hello', 'World'];
---
+++ aula1/inference.ts
@@ -0,0 +1,19 @@
+function showLogger(message: any)
+{
+    console.log(message);
+}
+
+showLogger("Hello world");
+showLogger(23);
+
+/*
+string
+number
+boolean
+array
+Class
+*/
+
+let text : Array<String> = ['Hello', 'Teste'];
+let isEnabled: boolean = true;
+let text2: [string, string] = ['Hello', 'World'];
