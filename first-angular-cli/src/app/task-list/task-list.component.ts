b065ab52fee52912e476271390e61433d837db35
Jeferson Job <jeferson.job@outlook.com>
2018-09-24 18:11:56 -0400

    Implements ngFor and create new components

---
+++ first-angular-cli/src/app/task-list/task-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  tasks = [];
+  task = "";
+  add():void
+  {
+    this.tasks.push(this.task);
+  }
+}
