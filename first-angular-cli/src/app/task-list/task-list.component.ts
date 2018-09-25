9884d3949cb6cd4af182b1b959bfbae191c23ade
Jeferson Job <jeferson.job@outlook.com>
2018-09-25 13:18:35 -0400

    New directives Angular and pipeline

--- first-angular-cli/src/app/task-list/task-list.component.ts
+++ first-angular-cli/src/app/task-list/task-list.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TaskListComponent {
   tasks = [];
   task = "";
+  isAdmin: boolean = true;
   add():void
   {
     this.tasks.push(this.task);
